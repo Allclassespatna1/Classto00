130ce1ec9f9cd44453d6872ca75b7e1e4ebd7c64
Allclassespatna1 <167908825+Allclassespatna1@users.noreply.github.com>
2024-06-03 08:15:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import re
 import os
 
 bot = Client("bot",
-             bot_token= "7201904058:AAEdKYHfCnYoNWOfDp3zUql40_wZZCBR-5M",
+             bot_token= "7251005777:AAHQ-qNJrZ1Wm683pu8-96pm6mqlTaBKxAc",
              api_id=21537501,
              api_hash= "02d8ef0eae2926ec4fd0cbff05ec0737")
 
