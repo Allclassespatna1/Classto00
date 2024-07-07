8c64df3a296acbee48dd9538b691533a16d3db94
Allclassespatna1 <167908825+Allclassespatna1@users.noreply.github.com>
2024-07-07 08:54:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ API_ID = API_ID = 21537501
 
 API_HASH = os.environ.get("API_HASH", "02d8ef0eae2926ec4fd0cbff05ec0737")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6997850062:AAHWrgAFDhUqJZ_ymrJKxlH1N--HvjNcyxE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7411835985:AAFwinmwg-WSw7C5SJC6qN81U0TV2bLT5B8")
 
 PASS_DB = int(os.environ.get("PASS_DB", "721"))
 
